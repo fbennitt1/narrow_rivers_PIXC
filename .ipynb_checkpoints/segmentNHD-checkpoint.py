6b51a0bf7a2fa34d967a964a314005665ddeda5d
fbennitt1 <fbennitt@umass.edu>
2025-01-14 20:14:25 +0000

    Close to having coverage stats for HUCs, but I need to calculate overall coverage.

---
+++ .ipynb_checkpoints/segmentNHD-checkpoint.py
@@ -0,0 +1,42 @@
+import geopandas as gpd
+import os
+import pandas as pd
+
+from reaches import readNHD
+from reaches import cut
+from reaches import segmentReach
+
+'''
+'''
+# Get slurm job index
+slurm = int(os.environ['SLURM_ARRAY_TASK_ID'])
+
+## Prepare data
+# Read prepped NHD
+basin, huc4, huc2 = readNHD(index=slurm)
+
+# Segment the reaches
+basin[['segments', 'failed']] = basin.parallel_apply(func=segmentReach, axis=1).apply(pd.Series)
+
+# Keep only reaches that were sucessfully segmented
+basin = basin[basin['failed'] == 0]
+
+# Drop failure column
+basin = basin.drop(columns='failed')
+
+# Explode the segments to give each own row
+basin =  basin.explode(column='segments', index_parts=True)
+
+# Drop original reach geometry column, set segments as active geometry
+basin = basin.drop(columns='geometry').set_geometry('segments').set_crs(crs=basin.crs)
+
+# Write out
+# Set write filepath
+save_path = '../narrow_rivers_PIXC_data/NHD_prepped_segmented/'
+save_path = os.path.join(save_path, huc2)
+save_file = huc4 + '_prepped_segmented.parquet'
+
+#Write out gdf as parquet file
+if not os.path.isdir(save_path):
+    os.makedirs(save_path)
+basin.to_parquet(os.path.join(save_path, save_file))
